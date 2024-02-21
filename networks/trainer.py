f1165746f0196d107f81d75ef32ebdfdf02355a7
chuangchuangtan <chuangchuangtan@aliyun.com>
2024-02-21 17:38:32 +0800

    Update trainer.py

--- networks/trainer.py
+++ networks/trainer.py
@@ -13,9 +13,7 @@ class Trainer(BaseModel):
         super(Trainer, self).__init__(opt)
 
         if self.isTrain and not opt.continue_train:
-            self.model = resnet50()
-            self.model.fc = nn.Linear(2048, 1)
-            torch.nn.init.normal_(self.model.fc.weight.data, 0.0, opt.init_gain)
+            self.model = resnet50(pretrained=False, num_classes=1)
 
         if not self.isTrain or opt.continue_train:
             self.model = resnet50(num_classes=1)
@@ -24,10 +22,10 @@ class Trainer(BaseModel):
             self.loss_fn = nn.BCEWithLogitsLoss()
             # initialize optimizers
             if opt.optim == 'adam':
-                self.optimizer = torch.optim.Adam(self.model.parameters(),
+                self.optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, self.model.parameters()),
                                                   lr=opt.lr, betas=(opt.beta1, 0.999))
             elif opt.optim == 'sgd':
-                self.optimizer = torch.optim.SGD(self.model.parameters(),
+                self.optimizer = torch.optim.SGD(filter(lambda p: p.requires_grad, self.model.parameters()),
                                                  lr=opt.lr, momentum=0.0, weight_decay=0)
             else:
                 raise ValueError("optim should be [adam, sgd]")
