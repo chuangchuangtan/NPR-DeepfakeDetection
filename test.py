b4e1bfa59ec58542ab5b1e78a3b75b54df67f3b8
chuangchuangtan <chuangchuangtan@aliyun.com>
2024-04-25 15:45:11 +0800

    Update test.py

--- test.py
+++ test.py
@@ -10,8 +10,18 @@ from options.test_options import TestOptions
 import networks.resnet as resnet
 import numpy as np
 import random
-
-
+import random
+def seed_torch(seed=1029):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed) # if you are using multi-GPU.
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.enabled = False
+seed_torch(100)
 DetectionTests = {
                 'ForenSynths': { 'dataroot'   : '/opt/data/private/DeepfakeDetection/ForenSynths/',
                                  'no_resize'  : False, # Due to the different shapes of images in the dataset, resizing is required during batch detection.
